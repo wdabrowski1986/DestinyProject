6be278927e5100e4facb4638f1d99a8edb37cccb
wdabrowski1986 <58408213+wdabrowski1986@users.noreply.github.com>
2020-07-12 14:40:21 -0500

    saving changes

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -10,24 +10,47 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//db
-mongoose.connect(process.env.MONGO_URI,
-    {useNewUrlParser: true, useUnifiedTopology: true},
-    )
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true
+    })
     .then(() => console.log('DB Connected'));
 
 mongoose.connection.on('error', err => {
-    console.log(`DB Connection error: ${err.message}`);
+    console.log(`DB connection error: ${err.message}`);
 });
 
+// bring in routes
+const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+// apiDocs
+app.get('/api', (req, res) => {
+    fs.readFile('docs/apiDocs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
 
-//bring in routes
-const postRoutes = require("./routes/post");
-
-
-//middleware
+// middleware -
 app.use(morgan('dev'));
-app.use("/", postRoutes);
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use('/api', postRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: 'Unauthorized!' });
+    }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
