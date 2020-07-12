e1f59636a4f2795e99f70f0f4e3866db1e6ddbfe
wdabrowski1986 <58408213+wdabrowski1986@users.noreply.github.com>
2020-07-12 13:39:21 -0500

    ff

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,5 +1,13 @@
+const Post = require("../models/post");
+
 exports.getPosts=(req, res) => {
     res.json({
-        posts: [{title: 'First post'}, {title: 'Second post'}]
+        posts: [{title: "First post"}, {title: "Second post"}]
     });
+};
+
+
+exports.createPost = (req, res) => {
+    const post = new Post(req.body)
+    console.log("CREATING POST: ", post);
 };
