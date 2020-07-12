613e1a5a0e3a3c2c3d08119f081efba59fb7277d
wdabrowski1986 <58408213+wdabrowski1986@users.noreply.github.com>
2020-07-12 13:13:03 -0500

    connected DB

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//db
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB Connection error: ${err.message}`);
+});
+
 
 //bring in routes
-const {getPosts} = require("./routes/post");
+const postRoutes = require("./routes/post");
 
 
 //middleware
 app.use(morgan('dev'));
+app.use("/", postRoutes);
 
-
-app.get("/", getPosts);
-
-const port = 8080
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node Js API is listening on port: ${port}`);
 });
